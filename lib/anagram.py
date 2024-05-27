fdc824633370a4ed5afeb3b6d497a77d876d547e
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-27 07:34:18 +0300

    passes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if sorted(anagram) == self.sorted_word and anagram.lower()!= self.word.lower()]
+
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))  # => ['inlets']
